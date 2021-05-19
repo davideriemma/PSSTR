f964081ec1e67fdacef967948d36a02c24f33196
Davide Riemma <riemma.d@gmail.com>
2021-05-19 23:21:11 +0200

    Completed some TBDs

--- Esercizi_ed_Esperimenti/Task_Userspace/task_info.h
+++ Esercizi_ed_Esperimenti/Task_Userspace/task_info.h
@@ -1,1 +1,8 @@
 /**informazioni condivise*/
+
+#ifndef _TASK_INFO_H_
+#define _TASK_INFO_H_
+
+#define SHARED_MEMORY_NAME 240698
+
+#endif
--- Esercizi_ed_Esperimenti/Task_Userspace/thread.c
+++ Esercizi_ed_Esperimenti/Task_Userspace/thread.c
@@ -1,9 +1,10 @@
 #include <string.h> //for strncpy
-#include <stdio.h> //for printf
+#include <stdio.h> //for printf>
 #include <rtai_lxrt.h> //fort rtai userspace related tasks
 #include <rtai_shm.h>
 
 #include "thread.h"
+#include "task_info.h"
 
 static RT_TASK * power_presence_checker_task;
 
@@ -14,14 +15,17 @@ void power_presence_checker_init_struct(power_failure_t * data)
     return;
 }
 
-unsigned int has_power_failded(void) //simulates random power failure, then returns 1 if the power has failed
+unsigned int has_power_failed(void) //simulates random power failure, then returns 1 if the power has failed
 {
-    //TBD: simulare un power failure
-    return 0;
+    //TBD: vedere se funziona -> Shifta a sinistra finquando non arriva a 0. Fa questo per 64 volte
+    static unsigned int i = 1;
+    i <<= 1;
+    return i;
 }
 
-void * power_presence_checker(void *)
+void * power_presence_checker(void * dummy)
 {
+    char message[] = "Power Has failed, please, intervene! 40 minutes to go";
     //inizializza il task
     if(!(power_presence_checker_task = rt_task_init_schmod(nam2num("PWCHK"), 5, 0, 0, SCHED_FIFO, 0)))
     {
@@ -30,8 +34,9 @@ void * power_presence_checker(void *)
     }
     else
     {
-        //TBD: acquisizione del puntatore alla shmem
+        //TBD: acquisizione del puntatore alla shmem -> done
         //nota: la shmem contiene sia il dato, che il flag di terminazione
+        power_failure_t * data = rt_shm_alloc(SHARED_MEMORY_NAME, sizeof(power_failure_t), USE_VMALLOC);
 
         //attiva il task periodico
         rt_task_make_periodic_relative_ns(power_presence_checker_task, 0, nano2count(T_c));
@@ -39,13 +44,22 @@ void * power_presence_checker(void *)
         rt_make_hard_real_time();
 
         /*task vero e proprio*/
-        while(!has_power_failded()){/*do nothing*/}
+        while(!has_power_failed()){/*do nothing*/}
 
-        /*TBD: power has failed, procedi*/
+        /*TBD: power has failed, procedi -> done*/
+        data->flag = 1;
+        
+        //la funzione di copia la si implementa a mano, onde evitare system calls (che non so perché debbano)
+        //esserci in una funzione come strncpy, ma non si può mai sapere
+
+        for(int i = 0; i < sizeof(message); ++i) //copia anche \0.
+        {
+            data->message[i] = message[i];
+        }
 
         /*operazioni di pulizia*/
-        //TBD procedere con la chiusura delle shmem
-
+        //TBD procedere con la chiusura delle shmem ->done
+        rt_shm_free(SHARED_MEMORY_NAME);
         //eliminazione del tasks
         rt_task_delete(power_presence_checker_task);
 
